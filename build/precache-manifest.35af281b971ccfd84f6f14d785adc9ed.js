67ae12ca95e2abe132ae41247cf2fbe0fd96927c
eatenapplee <haider.alkaleedy@edu.turkuamk.fi>
2020-07-25 13:06:35 +0300

    added build

---
+++ build/precache-manifest.35af281b971ccfd84f6f14d785adc9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb693a1e920e7d690e1ed47cc484983c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b16610a8028ddbad6e",
+    "url": "/static/css/main.7ba29781.chunk.css"
+  },
+  {
+    "revision": "f5f7b13d34d14b56302e",
+    "url": "/static/js/2.076799b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.076799b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b16610a8028ddbad6e",
+    "url": "/static/js/main.bfe9f6ce.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
